f5f656549c440a063ff49e6fc15ab8aa7b043356
HilmiKilavuz <kilavuzhilmi@gmail.com>
2025-11-25 18:57:20 +0300

    vercele uygun hale getirmeye  çalışıyorum

--- src/utils/lenis.tsx
+++ src/utils/lenis.tsx
@@ -1,5 +1,10 @@
 // src/utils/lenis.tsx
 "use client";
+
 import { ReactLenis as Lenis } from "@studio-freight/react-lenis";
 
-export const ReactLenis = Lenis;
+// TypeScript hatasını çözmek için props tipini 'any' yapıyoruz.
+// Bu, "Gelen verinin tipi ne olursa olsun kabul et" demektir.
+export function ReactLenis(props: any) {
+  return <Lenis {...props} />;
+}
